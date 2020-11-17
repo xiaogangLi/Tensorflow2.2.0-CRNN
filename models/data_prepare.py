f7b07f07bf71251a586880ecbe468ce245a81f54
lvjianjin <1399230759@qq.com>
2020-11-17 04:24:46 +0000

    增加数据集保存的方法

--- models/data_prepare.py
+++ models/data_prepare.py
@@ -1,9 +1,10 @@
 import os
 import cv2
 import random
+import pickle
 from pathlib import Path
 import tensorflow as tf
-from models.config import TABLE_PATH, NUM_CLASSES,OUTPUT_SHAPE
+from models.config import TABLE_PATH, NUM_CLASSES,OUTPUT_SHAPE, WORK_PATH
 
 #字典查询器
 table = tf.lookup.StaticHashTable(
@@ -58,29 +59,39 @@ def get_image_path(dir_path):
     '''
     获取图片路径列表,及其标签列表
     '''
-    images  = []
-    train_all_image_paths = []
-    train_all_image_labels = []
-    val_all_image_paths = []
-    val_all_image_labels = []
-    for root, dirs, files in os.walk(dir_path):
-        for file in files:
-            if '.jpg' in file:
-                file_path = os.path.join(root, file)
-                label_path = file_path.replace('.jpg','.txt')
-                if Path(file_path.replace('.jpg','.txt')).exists():
-                    with open(label_path) as f:
-                        label = f.read().strip()
-                    imgs= cv2.imread(file_path)
-                    if imgs.shape[1]/imgs.shape[0] <= 10 and len(label)>0:
-                        images.append((file_path, label))
-    random.shuffle(images)
-    for image,label in images:
-        random_num = random.randint(1,80)
-        if random_num == 5:
-            val_all_image_paths.append(image)
-            val_all_image_labels.append(label)
-        else:
-            train_all_image_paths.append(image)
-            train_all_image_labels.append(label)
-    return train_all_image_paths, train_all_image_labels,val_all_image_paths,val_all_image_labels
+    if os.path.exists(WORK_PATH+'dataset/dataset.data'):
+        with open(WORK_PATH+'dataset/dataset.data', 'rb') as f:
+            train_all_image_paths, train_all_image_labels,val_all_image_paths,val_all_image_labels = pickle.load(f)
+        print('数据集加载完毕！')
+        return train_all_image_paths, train_all_image_labels,val_all_image_paths,val_all_image_labels
+    else:
+        print('开始获取数据集，请耐心等待...')
+        images  = []
+        train_all_image_paths = []
+        train_all_image_labels = []
+        val_all_image_paths = []
+        val_all_image_labels = []
+        for root, dirs, files in os.walk(dir_path):
+            for file in files:
+                if '.jpg' in file:
+                    file_path = os.path.join(root, file)
+                    label_path = file_path.replace('.jpg','.txt')
+                    if Path(file_path.replace('.jpg','.txt')).exists():
+                        with open(label_path) as f:
+                            label = f.read().strip()
+                        imgs= cv2.imread(file_path)
+                        if imgs.shape[1]/imgs.shape[0] <= 10 and len(label)>0:
+                            images.append((file_path, label))
+        random.shuffle(images)
+        for image,label in images:
+            random_num = random.randint(1,80)
+            if random_num == 5:
+                val_all_image_paths.append(image)
+                val_all_image_labels.append(label)
+            else:
+                train_all_image_paths.append(image)
+                train_all_image_labels.append(label)
+        with open(WORK_PATH+'dataset/dataset.data', 'wb') as f:
+            pickle.dump((train_all_image_paths, train_all_image_labels,val_all_image_paths,val_all_image_labels), f)
+        print('数据集加载完毕！')
+        return train_all_image_paths, train_all_image_labels,val_all_image_paths,val_all_image_labels
