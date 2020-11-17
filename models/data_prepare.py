213bad72cf2982a5385d6548772ffd1e7fe46062
lvjianjin <1399230759@qq.com>
2020-11-17 08:27:19 +0000

    增加数据增强方法

--- models/data_prepare.py
+++ models/data_prepare.py
@@ -18,25 +18,25 @@ table = tf.lookup.StaticHashTable(
 
 #数据预处理方法
 def preprocess_image(image, mode = 'train'):
-    if mode == 'train':
-        image_shape = (32, 320, 3)
-    else:
-        image_shape = (32, 720, 3)
     image = tf.image.decode_jpeg(image, channels=3)
-    imgH, imgW, imgC = image_shape
     image = image / 255
     image -= 0.5
     image /= 0.5
-    # 饱和度
-    image = tf.image.random_saturation(image, 0.1, 5)
-    # 色调
-    image = tf.image.random_hue(image, 0.2)
-    # 对比度
-    image = tf.image.random_contrast(image, 0.2, 3)
-    # 亮度
-    image = tf.image.random_brightness(image, max_delta=0.5)
-    # 随机噪声
-    image = tf.image.random_jpeg_quality(image,0,100)
+    if mode == 'train':
+        image_shape = (32, 320, 3)
+        # 饱和度
+        image = tf.image.random_saturation(image, 0.1, 5)
+        # 色调
+        image = tf.image.random_hue(image, 0.2)
+        # 对比度
+        image = tf.image.random_contrast(image, 0.2, 3)
+        # 亮度
+        image = tf.image.random_brightness(image, max_delta=0.5)
+        # 随机噪声
+        image = tf.image.random_jpeg_quality(image,0,100)
+    else:
+        image_shape = (32, 720, 3)
+    imgH, imgW, imgC = image_shape
     resized_image = tf.image.resize(image, [imgH, imgW],preserve_aspect_ratio=True)
     padding_im = tf.image.pad_to_bounding_box(resized_image,0,0,imgH, imgW)
     return padding_im
